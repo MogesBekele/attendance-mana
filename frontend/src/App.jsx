7d7c8c4f1bc2d29515a34d389ef22a2550f78180
MogesBekele <mogesbekele32@gmail.com>
2025-02-06 12:30:09 +0300

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Register from './components/Register'
 
 const App = () => {
   return (
     <div>
       <h2>helllo world</h2>
+      <Register/>
     </div>
   )
 }
